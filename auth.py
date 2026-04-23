0bf694855e24cbf60f17755484dba7c17f6497e2
Andres Araya <andres.araya.c@ug.uchile.cl>
2026-04-23 12:18:59 -0400

    a

--- auth.py
+++ auth.py
@@ -1,20 +1,39 @@
 import json
 import pyotp
 
+print("¡Bienvenido a la plataforma de servicio al cliente de la tienda TC5G! ")
+print("Para autenticarse ingrese su mail (username) y contraseña:")
+
 with open("accounts.json") as f:
     accounts = json.load(f)
 
-username = input("Username: ")
-password = input("Password: ")
-code     = input("2FA Code: ")
+Login = False
 
-account = next((a for a in accounts if a["username"] == username), None)
+while not Login:
+    # Bucle para el usuario
+    while True:
+        username = input("Username: ")
+        account = next((a for a in accounts if a["username"] == username), None)
 
-if not account:
-    print("Authentication failed")
-elif account["password"] != password:
-    print("Authentication failed")
-elif not pyotp.TOTP(account["secret"]).verify(code, valid_window=1):
-    print("Authentication failed")
-else:
-    print(f"Authentication successful! Welcome, {account['name']}.")
+        if not account:
+            print("Authentication failed\n")
+        else:
+            break  # Usuario correcto
+
+    # Bucle para la contraseña
+    while True:
+        password = input("Password: ")
+
+        if account["password"] != password:
+            print("Authentication failed\n")
+        else:
+            break  # Contraseña correcta
+
+    # Solo se llega aquí con usuario y contraseña válidos
+    code = input("2FA Code: ")
+
+    if not pyotp.TOTP(account["secret"]).verify(code, valid_window=1):
+        print("Authentication failed\n")
+    else:
+        Login = True
+        print(f"Authentication successful! Welcome, {account['name']}.")
